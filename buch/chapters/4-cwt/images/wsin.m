ed7f67cd8cd9440323e669ebdfe87eff7918a941
Andreas Müller <andreas.mueller@othello.ch>
2019-02-11 09:25:52 +0100

    Umkehrformel, wsin.pdf Beispiel

--- buch/chapters/4-cwt/images/wsin.m
+++ buch/chapters/4-cwt/images/wsin.m
@@ -7,9 +7,9 @@ bmin = -2;
 bmax = 12;
 bstep = 0.1;
 
-amin = 0.1;
-astep = 0.1;
-amax = 5;
+amin = 0.25;
+astep = 0.5;
+amax = 30;
 
 f = fopen("wsindots.tex", "w");
 
@@ -17,7 +17,7 @@ for (b = (bmin:bstep:bmax))
 	for (a = (amin:astep:amax))
 		w = (2 * cos(0.5*a+b) - cos(a + b) - 1)/sqrt(a);
 		w = round(100 * abs(w));
-		fprintf(f, "\\punkt{%.1f}{%.1f}{%.0f}\n", a, b, w);
+		fprintf(f, "\\punkt{%.3f}{%.3f}{%.0f}\n", a/5, b, w);
 	endfor
 endfor
 
