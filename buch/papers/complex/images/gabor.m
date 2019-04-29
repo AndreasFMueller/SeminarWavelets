f9f75f1c952594d2ee722ea1f8c7b4cbd0c70b26
Ayexor <roy.seitz@epost.ch>
2019-04-29 20:39:18 +0200

    Skalierung korrigiert

--- buch/papers/complex/images/gabor.m
+++ buch/papers/complex/images/gabor.m
@@ -1,17 +1,19 @@
 # (C) 2019 Roy Seitz
 # Generate data points for a Gabor-Wavelet
 
-N = 200;
+N = 500;
 t = linspace(-4, 4, N)';
 
-h = exp(-1/2*t.^2) .* cos(2 * pi * t);
+env = exp(-1/2*t.^2);
+h = env .* cos(2 * pi * t);
+env = env / sqrt((sum(h'*h)*mean(diff(t)))); % Normalize to ||psi|| = 1
 h = h ./ sqrt((sum(h'*h)*mean(diff(t)))); % Normalize to ||psi|| = 1
 
-%plot(t, h); grid on;
+plot(t, [h, env, -env]); grid on;
 
 fp = fopen('./gabor.data', 'w');
-fprintf(fp, 't h\n');
+fprintf(fp, 't h envp envn\n');
 for i = 1:N
-  fprintf(fp, '%6.5f %6.5f\n', t(i), h(i));
+  fprintf(fp, '%6.5f %6.5f %6.5f %6.5f\n', t(i), h(i), env(i), -env(i));
 end
 fclose(fp);
--- buch/papers/complex/images/haar.m
+++ buch/papers/complex/images/haar.m
@@ -1,18 +1,19 @@
 # (C) 2019 Roy Seitz
 # Generate data points for a Haar-Wavelet and its hilbert transform
 
-n = 50; N = 6 * n;
+n = 200; N = 6 * n;
 t = linspace(-1, 2, N)';
 
-h = sqrt(2) * [zeros(2*n, 1); ones(n,1); -ones(n, 1); zeros(2*n, 1)];
+h = 1 / sqrt(2) * [zeros(2*n, 1); ones(n,1); -ones(n, 1); zeros(2*n, 1)];
 
-hh = sqrt(2) / pi * log( abs((t .* (t - 1) ) ./ (2 * (t - 1/2).^2)));
+hh = 1 / (sqrt(2) * pi) * log( abs((t .* (t - 1) ) ./ (t - 1/2).^2));
 
+f = h + 1i * hh;
 % plot(t, [h, hh]); xlim([-1, 2]); ylim([-2, 2]);
 
 fp = fopen('./haar.data', 'w');
-fprintf(fp, 't h hh\n');
+fprintf(fp, 't im envp envn\n');
 for i = 1:N
-  fprintf(fp, '%6.5f %6.5f %6.5f\n', t(i), h(i), hh(i));
+  fprintf(fp, '%6.5f %6.5f %6.5f %6.5f\n', t(i), hh(i), abs(f(i)), -abs(f(i)));
 end
 fclose(fp);
