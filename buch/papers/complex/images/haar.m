a23b711c6e4f9e3510ffdf65bb555f809b3bc44a
Ayexor <roy.seitz@epost.ch>
2019-05-04 16:06:54 +0200

    Haar-Wavelet zentriert

--- buch/papers/complex/images/haar.m
+++ buch/papers/complex/images/haar.m
@@ -2,14 +2,14 @@
 # Generate data points for a Haar-Wavelet and its hilbert transform
 
 n = 200; N = 6 * n;
-t = linspace(-1, 2, N)';
+t = linspace(-1.5, 1.5, N)';
 
 h = 1 / sqrt(2) * [zeros(2*n, 1); ones(n,1); -ones(n, 1); zeros(2*n, 1)];
 
-hh = 1 / (sqrt(2) * pi) * log( abs((t .* (t - 1) ) ./ (t - 1/2).^2));
+hh = 1 / (sqrt(2) * pi) * log( abs((t .^2 - 0.25 ) ./ t.^2));
 
 f = h + 1i * hh;
-% plot(t, [h, hh]); xlim([-1, 2]); ylim([-2, 2]);
+plot(t, [h, hh]); xlim([-1.5, 1.5]); ylim([-1.5, 1.5]);
 
 fp = fopen('./haar.data', 'w');
 fprintf(fp, 't im envp envn\n');
